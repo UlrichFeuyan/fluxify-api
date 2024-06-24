e0add184b5314601299cf38de9bfed7a6d10b4b0
ulrich <ulrich.djofang@cesd-sa.com>
2024-06-24 18:30:36 +0100

    base config

--- core/settings.py
+++ core/settings.py
@@ -223,6 +223,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
